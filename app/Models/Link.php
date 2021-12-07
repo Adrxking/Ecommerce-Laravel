938bdef06ec5f18e6c7c938bcf5674834135de76
Adrianas Vitys <75079985+Adrxking@users.noreply.github.com>
2021-12-07 15:37:02 +0100

    links debug

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -42,4 +42,9 @@ class Link extends Model
     {
         return $this->belongsToMany(Product::class, LinkProduct::class);
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'code', 'code')->where('complete', 1);
+    }
 }
